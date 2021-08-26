86473d23d9f9ca23a354a89d9b742f89ddf44d45
Edwin van de Pol <developer@edwinvandepol.nl>
2021-08-26 19:26:01 +0200

    Revert "Split drivers"
    This reverts commit 6bbec7eea0a156d267eae748f5e4fb79afd7b330.

--- drivers/bridge/driver.js
+++ drivers/bridge/driver.js
@@ -1,39 +1,7 @@
 'use strict';
 
-const {OAuth2Driver} = require('homey-oauth2app');
+const Driver = require('/lib/Driver');
 
-class BridgeDriver extends OAuth2Driver {
-
-  /**
-   * Pair devices.
-   */
-  async onPairListDevices({oAuth2Client}) {
-    this.log(`Listing bridges`);
-
-    const availableDevices = await oAuth2Client.getDevicesDetails('bridge');
-
-    let devices = [];
-
-    // Loop available devices
-    availableDevices.forEach(device => {
-      devices.push({
-        name: device.name,
-        data: {
-          id: device.serialNumber
-        },
-        settings: {
-          status: device.isConnected ? this.homey.__('connected') : this.homey.__('disconnected'),
-          tedee_id: String(device.id),
-          firmware_version: String(device.softwareVersions[0].version),
-          serial_number: String(device.serialNumber),
-          mac_address: String(device.macAddress)
-        }
-      });
-    });
-
-    return devices;
-  }
-
-}
+class BridgeDriver extends Driver { }
 
 module.exports = BridgeDriver;
